812a551e3762747bf92a643af0023c67e7105562
Harrsh Patel <me.harrsh.patel@gmail.com>
2022-02-23 13:50:07 +0530

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Details from "./containers/Details";
+import Home from "./containers/Home";
 
 const App: React.FC = () => {
-    return <div>App</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/details" element={<Details />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
